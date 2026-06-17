3d38a4fd6d36ad642649a74bf646231104878a49
maslo <user@DESKTOP-K8VKTCU>
2026-06-17 18:51:44 +0200

    very much not working simulation

--- Source.cpp
+++ Source.cpp
@@ -16,9 +16,9 @@
 
 struct Cell {
 	glm::vec2 velocity = { 0.0f, 0.0f }; // m/s
-	float density = 0.0f;  // kg/m^2
-	float preassure = 0.0f; // Pa
-	float temperature = 0.0f; // K
+	float density = 1.0f;  // kg/m^2
+	float pressure = 1000.0f; // Pa
+	float temperature = 293.15f; // K
 	float padding;
 };
 
@@ -35,12 +35,23 @@ int main() {
 	// ===========================
 
 	// simulation setup
-	glm::ivec2 simSize = { 5, 5 };
+	glm::ivec2 cellCount = { 50, 50 };
+	glm::vec2 simSize = { 2, 2 }; // meters
+	float dt = 0.0f;
 
 	// window setup
 	glfwInit();
 	Window window;
+	window.setVsync(false);
 	window.setResizable(false);
+	window.setSize(0, 0, 800, 800);
+
+	// ImGui setup
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window.getContext(), true);
+	ImGui_ImplOpenGL3_Init("#version 460");
+	ImGui::StyleColorsDark();
 
 	// shaders setup
 	std::string shadersDirectory = "shaders/";
@@ -50,32 +61,60 @@ int main() {
 	renderProg.addShader(GL_FRAGMENT_SHADER, readFileToString(shadersDirectory + "render.frag"));
 	renderProg.compile();
 
-	// multidraw setup
+	ShaderProgram computeProg;
+	computeProg.addShader(GL_COMPUTE_SHADER, readFileToString(shadersDirectory + "air_flow-er.comp"));
+	computeProg.compile();
+
+	// draw setup
 	VAO vao;
 
 	// buffers setup
 	bool currentCellBuffer = false;
 	Buffer cellBuffer[2] = { 
-		Buffer(sizeof(Cell) * simSize.x * simSize.y), 
-		Buffer(sizeof(Cell) * simSize.x * simSize.y) 
+		Buffer(sizeof(Cell) * cellCount.x * cellCount.y),
+		Buffer(sizeof(Cell) * cellCount.x * cellCount.y)
 	};
 
-	for (int x = 0; x < simSize.x; x++) {
-		for (int y = 0; y < simSize.y; y++) {
+	for (int x = 0; x < cellCount.x; x++) {
+		for (int y = 0; y < cellCount.y; y++) {
 			Cell cell;
 
-			if (x < simSize.x / 2) {
-				cell.temperature = 500;
-				cell.density = 1;
-				cell.preassure = 100;
+			// Test 01 - stability test
+			//cell.temperature = 293.15f;
+			//cell.density = 1.225f;
+			//cell.pressure = 101325.0f;
+
+			// Test 02 - partial vacuum
+			//if (x < cellCount.x / 2) {
+			//	cell.temperature = 293.15f + ((float)rand() / (float)RAND_MAX * 2.0f - 1.0f) * 40.0f;
+			//	cell.density = 1.225f + ((float)rand() / (float)RAND_MAX * 2.0f - 1.0f) * 0.2f;
+			//	cell.pressure = 101325.0f + ((float)rand() / (float)RAND_MAX * 2.0f - 1.0f) * 1000.0f;
+			//}
+
+			// Test 04 - one way
+			cell.temperature = 293.15f;
+			cell.density = 1.225f;
+			cell.pressure = 101325.0f;
+			if (x < cellCount.x / 2) {
+				cell.velocity = {1, 0};
 			}
 
-			cellBuffer[currentCellBuffer].write(&cell, x * simSize.y + y, sizeof(cell));
+			cellBuffer[currentCellBuffer].write(&cell, (y * cellCount.x + x) * sizeof(Cell), sizeof(cell));
 		}
 	}
+	// Test 03 - temperature point
+	//Cell cell;
+	//cell.temperature = 1000.15f;
+	//cell.density = 1.225f;
+	//cell.pressure = 101325.0f;
+	//cellBuffer[currentCellBuffer].write(&cell, (50 * cellCount.y + 50) * sizeof(Cell), sizeof(cell));
+
+	cellBuffer[!currentCellBuffer].write(cellBuffer[currentCellBuffer].getPtr(), 0, cellBuffer[currentCellBuffer].getSize());
 
 	// loop setup
 	glEnable(GL_PROGRAM_POINT_SIZE);
+
+	Fence fence;
 
 	while (!window.shouldClose()) {
 		window.updateFormat();
@@ -84,16 +123,39 @@ int main() {
 		window.setViewportSize(1, 1, 0, 0);
 		window.setViewport();
 
-		renderProg.useProgram();
+		fence.wait(1000000000);
 
+		currentCellBuffer = !currentCellBuffer;
 		cellBuffer[currentCellBuffer].bind(GL_SHADER_STORAGE_BUFFER, 0, 0, cellBuffer[currentCellBuffer].getSize());
 		cellBuffer[!currentCellBuffer].bind(GL_SHADER_STORAGE_BUFFER, 1, 0, cellBuffer[!currentCellBuffer].getSize());
-		currentCellBuffer = !currentCellBuffer;
 
-		glUniform2i(glGetUniformLocation(renderProg.getID(), "simSize"), simSize.x, simSize.y);
+		computeProg.useProgram();
+		
+		glUniform2i(glGetUniformLocation(computeProg.getID(), "cell_count"), cellCount.x, cellCount.y);
+		glUniform2f(glGetUniformLocation(computeProg.getID(), "cell_size"), 1.0f / (float)cellCount.x, 1.0f / (float)cellCount.y);
+		glUniform1f(glGetUniformLocation(computeProg.getID(), "dt"), dt);
+		
+		computeProg.runCompute(cellCount.x * cellCount.y, 1, 1, GL_SHADER_STORAGE_BARRIER_BIT);
+
+		renderProg.useProgram();
+
+		glUniform2i(glGetUniformLocation(renderProg.getID(), "cell_count"), cellCount.x, cellCount.y);
 		glUniform2i(glGetUniformLocation(renderProg.getID(), "resolution"), window.getFormat()->width, window.getFormat()->height);
 
-		vao.draw(GL_POINTS, simSize.x * simSize.y);
+		vao.draw(GL_POINTS, cellCount.x * cellCount.y);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("settings");
+		ImGui::SliderFloat("delta time", &dt, 0, 0.0001, "%.7f");
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		fence.place();
 
 		window.swapBuffers();
 		glfwPollEvents();
--- shaders/render.vert
+++ shaders/render.vert
@@ -3,7 +3,7 @@
 struct Cell {
 	vec2 velocity; // m/s
 	float density;  // kg/m^2
-	float preassure; // Pa
+	float pressure; // Pa
 	float temperature; // K
 	float padding;
 };
@@ -12,26 +12,33 @@ layout(std430, binding = 0) buffer cellBuffer {
 	Cell cell[];
 };
 
-uniform ivec2 simSize;
+uniform ivec2 cell_count;
 uniform ivec2 resolution;
 
 out vec4 cellColor;
+out vec2 velocity;
+out vec2 pointPos;
 
 void main() {
 	uint id = gl_VertexID;
 
 	ivec2 coord = ivec2(
-        int(id) % simSize.x,
-        int(id) / simSize.x
+        int(id) % cell_count.x,
+        int(id) / cell_count.x
     );
 
-    vec2 uv = (vec2(coord) + 0.5) / vec2(simSize);
+    vec2 uv = (vec2(coord) + 0.5) / vec2(cell_count);
     vec2 position = uv * 2.0 - 1.0;
 
-	ivec2 pixelsPerCell = resolution / simSize;
+	ivec2 pixelsPerCell = resolution / cell_count;
 
 	gl_Position = vec4(position, 0.0, 1.0);
     gl_PointSize = pixelsPerCell.x;
 
-	cellColor = vec4(cell[id].preassure, cell[id].temperature, cell[id].density, 1);
+	cellColor = vec4(
+		0.0,//cell[id].pressure / (101325.0 * 2),
+		0.0,//cell[id].temperature / (293.15 * 2),
+		length(cell[id].velocity) / 2.0, //cell[id].density / (1.225 * 2),
+		1);
+	velocity = cell[id].velocity;
 }
